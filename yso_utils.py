c9f6602281ad32bea9bc40cd96f78fd9b69ebced
marmicolta <micoltam@umich.edu>
2025-08-04 16:29:25 -0500

    added Lstar to yso_ultis, finished init in main

--- yso_utils.py
+++ yso_utils.py
@@ -1,8 +1,11 @@
 import numpy as np
 import constants as con
 
+def get_Lstar(radius,temp):
+      return 4*np.pi*(radius*con.Rsun)**2 * con.sig_sb * (temp)**4
+
 def get_Lacc(mass,radius,mdot):
     return con.G * mass * con.Msun * (mdot*(con.Msun/con.yr)/(radius*con.Rsun))
 
-def get_Rsub(Lstar,Lacc =False, Tsub = 1500):
+def get_Rsub(Lstar,Lacc, Tsub = 1500):
         return np.sqrt( (Lstar+Lacc)*con.Lsun / (4*np.pi *Tsub) )
