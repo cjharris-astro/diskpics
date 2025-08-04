6c04b0fde42774f755cfe8236822c91695925a57
marmicolta <micoltam@umich.edu>
2025-08-04 15:22:22 -0500

    fixed typo in rsub func

--- yso_utils.py
+++ yso_utils.py
@@ -4,5 +4,5 @@ import constants as con
 def get_Lacc(mass,radius,mdot):
     return con.G * mass * con.Msun * (mdot*(con.Msun/con.yr)/(radius*con.Rsun))
 
-def get_Rsub(Lstar,Lacc =False, Tsub = 1500)
+def get_Rsub(Lstar,Lacc =False, Tsub = 1500):
         return np.sqrt( (Lstar+Lacc)*con.Lsun / (4*np.pi *Tsub) )
