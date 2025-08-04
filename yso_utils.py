bcd40c4a6d45757d53ecbfbe015ae0b9b40af80d
marmicolta <micoltam@umich.edu>
2025-08-04 15:21:04 -0500

    created fundtion for Lacc and sublimation radius

---
+++ yso_utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+import constants as con
+
+def get_Lacc(mass,radius,mdot):
+    return con.G * mass * con.Msun * (mdot*(con.Msun/con.yr)/(radius*con.Rsun))
+
+def get_Rsub(Lstar,Lacc =False, Tsub = 1500)
+        return np.sqrt( (Lstar+Lacc)*con.Lsun / (4*np.pi *Tsub) )
