daba5230950dc65e26ff3023a473da28b8f1fd8f
marmicolta <micoltam@umich.edu>
2025-08-07 09:07:55 -0500

    typo

--- diskpics/diskpics.py
+++ diskpics/diskpics.py
@@ -135,7 +135,7 @@ def plot_disk(disco,rout=1.*u.Rsun):
         circle = plt.Circle((0, 0), 1, color='k')
     else:
         circle = plt.Circle((0, 0), 1, color='orange')
-        
+
     plt.gca().add_patch(circle)
     plt.gca().set_aspect('equal')
 
@@ -145,6 +145,6 @@ def plot_disk(disco,rout=1.*u.Rsun):
 
     plt.plot(R/disco.radius,disco.scale_height)
 
-    plt.xlim(0/R/disco.radius[-1])
+    plt.xlim(0,R/disco.radius[-1])
 
     plt.show()
