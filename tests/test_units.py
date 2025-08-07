d407f35a591fe208a18e583a3d56419b49f23c44
marmicolta <micoltam@umich.edu>
2025-08-07 14:12:04 -0500

    created test unit for imputs

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pytest as pt
+import astropy.units as u
+
 # Check radius, mass, mdot, inner radius, temp, zeros/negs,
 # order of magnitudes, height, realistic mdot, accretion luminosity,
 # 
