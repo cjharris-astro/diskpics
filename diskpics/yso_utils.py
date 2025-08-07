8dad487658ebe7697a46b01f6797ca49ed2c1d53
marmicolta <micoltam@umich.edu>
2025-08-07 16:12:00 -0500

    typo

--- diskpics/yso_utils.py
+++ diskpics/yso_utils.py
@@ -13,7 +13,7 @@ def get_Lacc(mass,radius,mdot):
 
 def get_Rsub(Lstar, Tsub = 1500*u.K):
         # return np.sqrt( (Lstar.cgs+Lacc.cgs)/ (4*np.pi *Tsub) )
-        return np.sqrt( (Lstar.cgs)/ (4*np.pi*sig_sb**Tsub) ).decompose()
+        return np.sqrt( (Lstar.cgs)/ (4*np.pi*sig_sb*Tsub**4) )
 
 def magnetosphere():
     return print("Moduled under construction")
