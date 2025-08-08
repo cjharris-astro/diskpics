9c789aa0f04e04bab3684604c17f40b50be64b58
marmicolta <micoltam@umich.edu>
2025-08-07 19:46:53 -0500

    went over units in yso utils and main classes in diskpics

--- diskpics/yso_utils.py
+++ diskpics/yso_utils.py
@@ -9,49 +9,29 @@ def get_Lstar(radius,temp):
       return 4*np.pi*(radius.cgs)**2 * sig_sb * (temp)**4
 
 def get_Lacc(mass,radius,mdot):
-    return con.G * mass.cgs * (mdot.cgs/(radius.cgs))
+    return con.G.cgs * mass.cgs * (mdot.cgs/(radius.cgs))
 
 def get_Rsub(Lstar, Tsub = 1500*u.K):
         # return np.sqrt( (Lstar.cgs+Lacc.cgs)/ (4*np.pi *Tsub) )
         return 1/(4*np.pi) * np.sqrt( (Lstar.cgs)/ (sig_sb*Tsub**4) )
 
 def magnetosphere():
-    return print("Moduled under construction")
+    return print("Module under construction")
 
-def flared_temp_distribution(Lstar,Mstar,Rarray):
-    Rarray = Rarray*u.cm
-    Ls = Lstar.cgs
-    sigma_sb = con.sigma_sb.cgs
-    K_b = con.k_B.cgs
-    mu = 2.3
-    mh = con.u.cgs
-    G = con.G.cgs
-    Ms = Mstar.cgs
+def flared_temp_distribution(Lstar,Mstar,Rarray,mu = 2.3):
+   
+    one = (Lstar.cgs/(4*np.pi*(Rarray.cgs)**2*sig_sb))**2
+    two = con.k_B.cgs/(mu*con.u.cgs)
+    three = Rarray.cgs/(con.G.cgs*Mstar.cgs)
 
-    one = (Ls/(4*np.pi*Rarray**2*sigma_sb))**2
-    two = K_b/(mu*mh)
-    three = Rarray/(G*Ms)
+    return (one*two*three)**(1/7)
+    # return Lstar.cgs**(2/7)*(Rarray.cgs)**(-3/7)
 
-    Td = (one*two*three)**(1/7)
+def flared_disk_ScaleHeight(Mstar,Rarray,Tdisk, mu = 2.3):
 
-    # return Td.cgs
-    return Ls**(2/7)*(Rarray)**(-3/7)
+    cs = np.sqrt((con.k_B.cgs*Tdisk.cgs)/(mu*con.u.cgs))
 
-def flared_disk_ScaleHeight(Mstar,Rarray,Tdisk):
+    v_k = np.sqrt(con.G.cgs*Mstar.cgs/Rarray.cgs)
 
-    Rarray = Rarray*u.cm
-    mu = 2.3
-    mh = con.u.cgs
-    G = con.G.cgs
-    Ms = Mstar.cgs
-    K_b = con.k_B.cgs
-    Tdisk = Tdisk.value *u.K
-
-    cs = np.sqrt((K_b*Tdisk)/(mu*mh))
-
-    v_k = np.sqrt(G*Ms/Rarray)
-
-    H = cs/v_k*Rarray
-
-    return H.cgs
+    return (cs/v_k)*Rarray.cgs
      
